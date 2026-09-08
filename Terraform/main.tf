5ef4a9169c7e5ceb98a72bceb63165019fd73928
Srujal Desai <desaisrujal09@gmail.com>
2026-09-08 12:56:10 -0400

    Renamed directory

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -61,6 +61,7 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
+
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
