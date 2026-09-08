b027a46689ed0bb0045ad9466dcfe2d52279b46c
desaisrujal09 <desaisrujal09@gmail.com>
2026-09-08 16:28:22 -0400

    Update source_file path for lambda_function.py

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -173,7 +173,7 @@ resource "aws_iam_role_policy_attachment" "lambda_autosre_attach" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "${path.module}/Terraform/lambda_function.py"
+  source_file = "${path.module}/lambda_function.py"
   output_path = "${path.module}/lambda_function.zip"
 }
 
